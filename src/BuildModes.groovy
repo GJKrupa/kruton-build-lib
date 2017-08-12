155f0ecd888e3516f26366c66d5a7b362f0dc10c
Gerard Krupa <gerard.krupa@gmail.com>
2017-08-12 13:22:54 +0100

    Update version again

--- src/BuildModes.groovy
+++ src/BuildModes.groovy
@@ -8,29 +8,43 @@ void setBuildStatus(String message, String state) {
     ]);
 }
 
+def skip_build() {
+    String GIT_COMMIT_EMAIL = sh (
+            script: 'git --no-pager show -s --format=\'%ae\'',
+            returnStdout: true
+    ).trim()
+    return GIT_COMMIT_EMAIL.contains("jenkins_ci")
+}
+
 def mavenBuildWithRelease() {
     node {
         checkout scm
         stage ('Build and Deploy') {
             if (env.BRANCH_NAME == 'master') {
-                sshagent(['GitHub']) {
-                    try {
-                        sh 'mvn release:prepare'
-                        sh 'mvn release:perform'
-                    } catch (e) {
-                        sh 'mvn release:rollback'
-                        throw e
+                if (skip_build()) {
+                    echo 'Copying build state from previous build'
+                    if(!hudson.model.Result.SUCCESS.equals(currentBuild.rawBuild.getPreviousBuild()?.getResult())) {
+                        currentBuild.result = 'FAILURE'
+                    }
+                } else {
+                    sshagent(['GitHub']) {
+                        try {
+                            sh 'mvn release:prepare'
+                            sh 'mvn release:perform'
+                        } catch (e) {
+                            sh 'mvn release:rollback'
+                            throw e
+                        } finally {
+                            junit allowEmptyResults: true, testResults: 'target/surefire-reports/**/*.xml'
+                            setBuildStatus("Build complete", "SUCCESS")
+                        }
                     }
                 }
             } else {
                 sh 'mvn clean install'
+                junit allowEmptyResults: true, testResults: 'target/surefire-reports/**/*.xml'
+                setBuildStatus("Build complete", "SUCCESS")
             }
-        }
-        stage ('Publish Tests') {
-            junit allowEmptyResults: true, testResults: 'target/surefire-reports/**/*.xml'
-        }
-        stage ('Update GitHub Status') {
-            setBuildStatus("Build complete", "SUCCESS")
         }
     }
 }
