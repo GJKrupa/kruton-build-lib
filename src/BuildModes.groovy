d7e8fd644aa044220a79265ee914924fd22e5619
Gerard Krupa <krupagj@krupa.me.uk>
2017-08-20 19:21:28 +0100

    Let's try a configure block

--- src/BuildModes.groovy
+++ src/BuildModes.groovy
@@ -18,15 +18,7 @@ def skip_build() {
 
 def mavenBuildWithRelease() {
     node {
-        checkout([
-                $class: 'GitSCM',
-                branches: [[name: "*/${env.BRANCH_NAME}"]],
-                extensions: [
-                        [$class: 'CleanCheckout'],
-                        [$class: 'PruneStaleBranch'],
-                        [$class: 'LocalBranch', localBranch: '**']
-                ]
-        ])
+        checkout scm
         stage ('Build and Deploy') {
             if (env.BRANCH_NAME == 'master') {
                 if (skip_build()) {
